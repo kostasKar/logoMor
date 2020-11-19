ad7e44e9470462804f98cc962973aa1ebe5ca6fc
Kostas Karouzos <ck.karouzos@gmail.com>
2020-11-19 21:06:20 +0200

    separated the <, >, <=, >=, = correctly with regular expressions

--- sourceCodeStringPreparation.js
+++ sourceCodeStringPreparation.js
@@ -5,10 +5,17 @@
 
 function prepareSourceCodeText(inputStr){
 
+	inputStr = inputStr.toLowerCase();
 	inputStr = inputStr.replace("[", " [ ");
 	inputStr = inputStr.replace("]", " ] ");
 	inputStr = inputStr.replace("(", " ( ");
 	inputStr = inputStr.replace(")", " ) ");
+	inputStr = inputStr.replace("+", " + "); //TODO -
+	inputStr = inputStr.replace("*", " * ");
+	inputStr = inputStr.replace("/", " / ");
+	inputStr = inputStr.replace(/([<|>])([^=])/, " $1 $2"); //separate only standalone < or >
+	inputStr = inputStr.replace(/([<|>]=)/, " $1 ");
+	inputStr = inputStr.replace(/([^<|>])(=)/, "$1 $2 ");
 
 	return inputStr;
 }
