b5a071be6430d35209c4f91c049ec32db2595194
kostasKar <ck.karouzos@gmail.com>
2020-11-22 19:00:01 +0200

    Added demo program and button for it

---
+++ demoProgram.js
@@ -0,0 +1,21 @@
+var demoProgram =  
+';-----------Demo program--------------\n'+
+'\n'+
+';------Function-----------------------\n'+
+'TO drawRandomCurve :n\n'+
+'  repeat :n [ \n'+
+'  right rand :maxangle \n'+
+'  up rand :maxangle\n'+
+'  roll_right rand :maxangle\n'+
+'  forward rand :maxsize \n'+
+'  ]\n'+
+'END\n'+
+'\n'+
+';------Global Variables---------------\n'+
+';-Check auto generated sliders below--\n'+
+'make "maxsize 20\n'+
+'make "maxangle  36\n'+
+'\n'+
+';--Actual Drawing---------------------\n'+
+'setpensize 3\n'+
+'drawRandomCurve 150\n';
