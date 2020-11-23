968ab6d091e8bb020f9d41112093621f8c2c2e31
Kostas Karouzos <ck.karouzos@gmail.com>
2020-11-23 23:57:21 +0200

    Added another demo program

--- demoProgram.js
+++ demoProgram.js
@@ -1,7 +1,7 @@
 
 
 function getRandomDemoProgram(){
-	var demoPrograms = [demoProgram1, demoProgram2];
+	var demoPrograms = [demoProgram1, demoProgram2, demoProgram3];
 	var i = Math.floor(Math.random()*demoPrograms.length);
 	return demoPrograms[i];
 }
@@ -76,3 +76,58 @@ var demoProgram2 =
 'pendown \n' + 
 'sphere :radius/4 :segs \n';
 
+var demoProgram3 = 
+';----------Demo Bracelet--------------  \n' + 
+' \n' + 
+';------Functions---------------------- \n' + 
+'TO centeredCircle :r :n  \n' + 
+' make "segment 2*pi*:r/:n  \n' + 
+' penup  \n' + 
+' rt 90  \n' + 
+' fd :r  \n' + 
+' dn 90  \n' + 
+' bk :segment/2  \n' + 
+' pendown  \n' + 
+' repeat :n [  \n' + 
+'  fd :segment   \n' + 
+'  dn 360 / :n  \n' + 
+' ]  \n' + 
+' penup  \n' + 
+' fd :segment/2  \n' + 
+' dn 90  \n' + 
+' fd :r  \n' + 
+' lt 90 \n' + 
+' rr 180 \n' + 
+' pendown   \n' + 
+'END  \n' + 
+'  \n' + 
+'TO sphere :r :n  \n' + 
+' repeat :n/2 [  \n' + 
+'  centeredCircle :r :n  \n' + 
+'  rt 360/:n  \n' + 
+' ]  \n' + 
+' rt 180 \n' + 
+'END  \n' + 
+'  \n' + 
+'to fdWithSphere :d :n \n' + 
+' penup \n' + 
+' fd :d/2 \n' + 
+' pendown \n' + 
+' sphere :d/2 :n \n' + 
+' penup  \n' + 
+' fd :d/2 \n' + 
+' pendown \n' + 
+'end \n' + 
+' \n' + 
+';-----Global Variables------------- \n' + 
+';-see auto generated sliders below- \n' + 
+'make "radius 100 \n' + 
+'make "segs 5 \n' + 
+'make "segs 2*:segs \n' + 
+'make "jewels 6 \n' + 
+' \n' + 
+';----drawing----------------------- \n' + 
+'repeat :jewels [ \n' + 
+' fdWithSphere 2*pi*:radius/:jewels :segs \n' + 
+' rt 360/:jewels  \n' + 
+'] \n';
