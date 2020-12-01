dd2cae7ceab317e440c04497e22545f90e76a1be
Kostas Karouzos <ck.karouzos@gmail.com>
2020-12-01 03:12:46 +0200

    Wrote a 4x4 matrix multiplication function for improved speed. For real improvement maybe I could use a graphics accelerated library to do this

--- logoMatrixTransformations.js
+++ logoMatrixTransformations.js
@@ -48,9 +48,7 @@ function logoRotateZ(rad){
 
 
 function multiplyToLogoMatrix(m){
-  
-  lm = multiplyMatrices(lm, m);
-  
+  lm = multiply4x4Matrices(lm, m);
 }
 
 function applyLogoTransformationMatrix(){
@@ -81,57 +79,27 @@ function setlmZ(newz){
   lm[14] = newz;
 }
 
-// point • matrix
-function multiplyMatrixAndPoint(matrix, point) {
-  // Give a simple variable name to each part of the matrix, a column and row number
-  let c0r0 = matrix[ 0], c1r0 = matrix[ 1], c2r0 = matrix[ 2], c3r0 = matrix[ 3];
-  let c0r1 = matrix[ 4], c1r1 = matrix[ 5], c2r1 = matrix[ 6], c3r1 = matrix[ 7];
-  let c0r2 = matrix[ 8], c1r2 = matrix[ 9], c2r2 = matrix[10], c3r2 = matrix[11];
-  let c0r3 = matrix[12], c1r3 = matrix[13], c2r3 = matrix[14], c3r3 = matrix[15];
-  
-  // Now set some simple names for the point
-  let x = point[0];
-  let y = point[1];
-  let z = point[2];
-  let w = point[3];
-  
-  // Multiply the point against each part of the 1st column, then add together
-  let resultX = (x * c0r0) + (y * c0r1) + (z * c0r2) + (w * c0r3);
-  
-  // Multiply the point against each part of the 2nd column, then add together
-  let resultY = (x * c1r0) + (y * c1r1) + (z * c1r2) + (w * c1r3);
-  
-  // Multiply the point against each part of the 3rd column, then add together
-  let resultZ = (x * c2r0) + (y * c2r1) + (z * c2r2) + (w * c2r3);
-  
-  // Multiply the point against each part of the 4th column, then add together
-  let resultW = (x * c3r0) + (y * c3r1) + (z * c3r2) + (w * c3r3);
-  
-  return [resultX, resultY, resultZ, resultW];
-}
 
-//matrixB • matrixA
-function multiplyMatrices(matrixA, matrixB) {
-  // Slice the second matrix up into rows
-  let row0 = [matrixB[ 0], matrixB[ 1], matrixB[ 2], matrixB[ 3]];
-  let row1 = [matrixB[ 4], matrixB[ 5], matrixB[ 6], matrixB[ 7]];
-  let row2 = [matrixB[ 8], matrixB[ 9], matrixB[10], matrixB[11]];
-  let row3 = [matrixB[12], matrixB[13], matrixB[14], matrixB[15]];
-
-  // Multiply each row by matrixA
-  let result0 = multiplyMatrixAndPoint(matrixA, row0);
-  let result1 = multiplyMatrixAndPoint(matrixA, row1);
-  let result2 = multiplyMatrixAndPoint(matrixA, row2);
-  let result3 = multiplyMatrixAndPoint(matrixA, row3);
-
-  // Turn the result rows back into a single matrix
+function multiply4x4Matrices(a, b){
   return [
-    result0[0], result0[1], result0[2], result0[3],
-    result1[0], result1[1], result1[2], result1[3],
-    result2[0], result2[1], result2[2], result2[3],
-    result3[0], result3[1], result3[2], result3[3]
-  ];
+    b[0]*a[0]+b[1]*a[4]+b[2]*a[8]+ b[3]*a[12], 
+    b[0]*a[1]+b[1]*a[5]+b[2]*a[9]+ b[3]*a[13], 
+    b[0]*a[2]+b[1]*a[6]+b[2]*a[10]+b[3]*a[14], 
+    b[0]*a[3]+b[1]*a[7]+b[2]*a[11]+b[3]*a[15],
+
+    b[4]*a[0]+b[5]*a[4]+b[6]*a[8]+ b[7]*a[12], 
+    b[4]*a[1]+b[5]*a[5]+b[6]*a[9]+ b[7]*a[13], 
+    b[4]*a[2]+b[5]*a[6]+b[6]*a[10]+b[7]*a[14], 
+    b[4]*a[3]+b[5]*a[7]+b[6]*a[11]+b[7]*a[15],
+
+    b[8]*a[0]+b[9]*a[4]+b[10]*a[8]+ b[11]*a[12], 
+    b[8]*a[1]+b[9]*a[5]+b[10]*a[9]+ b[11]*a[13], 
+    b[8]*a[2]+b[9]*a[6]+b[10]*a[10]+b[11]*a[14], 
+    b[8]*a[3]+b[9]*a[7]+b[10]*a[11]+b[11]*a[15],
+
+    b[12]*a[0]+b[13]*a[4]+b[14]*a[8]+ b[15]*a[12], 
+    b[12]*a[1]+b[13]*a[5]+b[14]*a[9]+ b[15]*a[13], 
+    b[12]*a[2]+b[13]*a[6]+b[14]*a[10]+b[15]*a[14], 
+    b[12]*a[3]+b[13]*a[7]+b[14]*a[11]+b[15]*a[15]
+  ]
 }
-
-
-
