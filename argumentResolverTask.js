e302e55f7e4673b30e6cf6aba2724203d80d6216
Kostas Karouzos <ck.karouzos@gmail.com>
2020-12-15 01:57:08 +0200

    Added a documentation comment to the ArgumentResolverTask class

--- argumentResolverTask.js
+++ argumentResolverTask.js
@@ -8,6 +8,22 @@ function isArithmeticOperator(token){
 }
 
 
+/*
+   ArgumentResolverTask is the task created by any command that expects argument(s).
+   It's tryToTakeInput() method can accept variable values (local, global or static), string literals, numbers and operators
+   The first argument it consumes is considered the left hand operand of a possible operation.
+   Once the task has a left hand operand, it can be resolved, returning its value.
+   However, the interpreter will first make sure that the argumentResolverTask can not consume any more inputs before it resolves it
+   After the left hand operand, argumentResolverTask can accept an operator (arithmetic or comparison)
+   Note: The minus operator is also acceptable as the first argument, for negative values. In that case '0' is considered as left-hand operand and '-' as operator
+   After accepting an operator, the argumentResolverTask recursively opens another ArgumentResolverTask, 
+   whose return value will be considered it's right hand operand and the task will again be resolvable
+   This way, an arbitrary number of chained operations can be performed before the initial resolver returns a value to the calling task
+   If the task is expecting an operand and the token it receives is not one of the directly consumable cases, it does not consume it and it remains unresolvable
+   That token will probably trigger the creation of a command task that produces a return value. Once that task is resolved, 
+   it's return value will be then be consumable by the argumentResolverTask.
+ */
+
 
 class ArgumentResolverTask {
   
@@ -28,7 +44,7 @@ class ArgumentResolverTask {
     
     if (!this.leftArgumentAvailable){
       if (arg === ""){
-        throwError("Argument resolver was fed with the result of a non-number returning task");
+        throwError("Argument resolver was fed with the result of a no return value task");
         return false;
       }
       var varscope = variablesScopeStack[variablesScopeStack.length-1];
@@ -50,13 +66,13 @@ class ArgumentResolverTask {
       } else {
       	if (!isNaN(arg)){
       		this.leftArgument = Number(arg);
-      	} else if (arg === "-") { //negative number input with a space between '-' and the number
+      	} else if (arg === "-") { //negative number input
       		this.leftArgument = 0;
       		this.leftArgumentAvailable = true;
       		this.operator = "-";
       		this.operatorAvailable = true;
       		this.canBeResolved = false;
-      		var art = new ArgumentResolverTask();
+      		new ArgumentResolverTask();
       		return true;
       	} else {
       		return false;
@@ -70,7 +86,7 @@ class ArgumentResolverTask {
         this.operator = arg;
         this.operatorAvailable = true;
         this.canBeResolved = false;
-        var art = new ArgumentResolverTask();
+        new ArgumentResolverTask();
         return true;
       } else {
         return false;
