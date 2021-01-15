8d00438518da7534ec3f916ea9ba200442238807
Kostas Karouzos <ck.karouzos@gmail.com>
2021-01-15 14:27:50 +0200

    Added auto- indentation when opening square brackets

--- codemirrorLogomor/mode/logomor/logomor.js
+++ codemirrorLogomor/mode/logomor/logomor.js
@@ -3,10 +3,53 @@ var moves = ["forward","fd","back","bk","right","rt","left","lt","up","down","dn
 var keywords = ["penup","pu","pendown","pd","showturtle","st","hideturtle","ht","setpensize","sps","settextsize","sts","color","coloralpha","getx","gety","getz","print","label","and","or","not","rand","random","randcrazy","randomcrazy","sqrt","power","pow","modulo","mod","cos","sin","tan","arccos","arcsin","arctan","ln","log","exp","pi","time","frame","int","round","trunc","abs","min","max","beginface","endface","mousex","mousey","mousepressed","thing","valueof","increment","decrement","point","dist"];
 var builtins = ["make","static","if","ifelse","repeat","repcount","while","until","to","end","return"];
 
-CodeMirror.defineMode("logomorMode", function() {
 
+
+
+
+
+
+
+
+
+
+
+(function(mod) {
+  if (typeof exports == "object" && typeof module == "object") // CommonJS
+    mod(require("../../lib/codemirror"));
+  else if (typeof define == "function" && define.amd) // AMD
+    define(["../../lib/codemirror"], mod);
+  else // Plain browser env
+    mod(CodeMirror);
+})(function(CodeMirror) {
+  "use strict";
+
+
+
+
+CodeMirror.defineMode("logomorMode", function(config) {
+  var indentUnit = config.indentUnit;
   return {
+
+
+    startState: function() {
+        return {
+          indentation: 0,
+        };
+      },
+
+    indent: function(state, textAfter){
+      if ((textAfter) && (textAfter.charAt(0) === "]")){
+        return state.indentation - indentUnit;
+      } else {
+        return state.indentation;
+      }
+      
+    },
+
     token: function(stream, state) {
+
+      state.indentation = stream.indentation();
 
       var ch = stream.peek();
       if (ch == ";") {
@@ -24,6 +67,11 @@ CodeMirror.defineMode("logomorMode", function() {
         stream.next();
         return "operator";
       } else if (ch.match(/[\[\]\(\)]/)){
+        if (ch === "["){
+          state.indentation += indentUnit;
+        } else if (ch === "]"){
+          state.indentation -= indentUnit;
+        }
         stream.next();
         return "enclosing";
       }
@@ -48,8 +96,9 @@ CodeMirror.defineMode("logomorMode", function() {
       return null;
 
     }
+
   };
 
 });
 
-
+});
