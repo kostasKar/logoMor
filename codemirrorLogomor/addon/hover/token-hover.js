9f8f8fe8ec18ab68e837590300ce45de748e6c73
Kostas Karouzos <ck.karouzos@gmail.com>
2021-11-03 19:07:26 +0200

    Added an Easter egg: When the user hovers on the command 'rand' or 'random', they can check the current seed for the RNG. That way, if the sketch has drawn a very nice random shape, the user can check the seed number and apply it with the 'randseed' command to reproduce the same output on subsequent executions

--- codemirrorLogomor/addon/hover/token-hover.js
+++ codemirrorLogomor/addon/hover/token-hover.js
@@ -5,6 +5,10 @@ CodeMirror.registerHelper("textHover", "logomorMode", function(cm, data, node) {
     var tokenName = (token.string) ? token.string.toLowerCase() : null;
     text += '<b>' + token.type + '</b> ' + tokenName;
     if (tokenName in commandHints) {text += '<br>' + commandHints[tokenName];}
+    //Easter egg for random number generator:
+    if ((tokenName === "rand") || (tokenName === "random")){
+    text += "<br>(Current seed: " + seed + ")";
+    }
   }
   var result = document.createElement('div');
   result.innerHTML = text;
