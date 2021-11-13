ddc5d72d55517dc7c4e1442de947af046249d12e
Kostas Karouzos <ck.karouzos@gmail.com>
2021-11-13 21:31:39 +0200

    minor correction and refactoring to the regexs of the code string preparation. No changes

--- sourceCodeStringPreparation.js
+++ sourceCodeStringPreparation.js
@@ -6,12 +6,11 @@
 function prepareSourceCodeText(inputStr){
 
 	inputStr = inputStr.toLowerCase();
-	inputStr = inputStr.replace(/([\[\]\(\)\+\-\*\/])/g, " $1 ");
-	inputStr = inputStr.replace(/([<|>])([^=])/g, " $1 $2"); //separate only standalone < or >
-	inputStr = inputStr.replace(/([<|>]=)/g, " $1 ");
-	inputStr = inputStr.replace(/([^<|>])(=)/g, "$1 $2 ");
-	inputStr = inputStr.replace(/;.*$/g, " ");
-	inputStr = inputStr.replace(/;.*\n/g, " ");
+	inputStr = inputStr.replace(/([\[\]\(\)\+\-\*\/])/g, " $1 "); //isolate operators and enclosures
+	inputStr = inputStr.replace(/([<>])([^=])/g, " $1 $2");       //isolate standalone <, >
+	inputStr = inputStr.replace(/([<>]=)/g, " $1 ");              //isolate <=, >=
+	inputStr = inputStr.replace(/([^<>])(=)/g, "$1 $2 ");         //isolate standalone =
+	inputStr = inputStr.replace(/;.*$/gm, " ");                   //remove comments
 
 	return inputStr;
 }
