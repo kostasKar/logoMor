74f34d66c2efc32a0e926e8eb8058d66f3881b00
Kostas Karouzos <ck.karouzos@gmail.com>
2025-06-20 21:58:53 +0300

    Update logomorApp.js

--- js/logomorApp.js
+++ js/logomorApp.js
@@ -1,1 +1,8 @@
+/*
+    LM is the object that contains the whole logomor functionality
+    All modules are created as attributes of LM object
+    Before minification, all source files are concatenated after this file
+*/
+
+
 var LM = {};
